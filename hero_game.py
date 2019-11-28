850a2af09376453483f29c12d0cd5e5231cbd757
janice-cotcher <janice.cotcher@rbe.sk.ca>
2019-11-28 09:04:10 -0600

    update hero_game.py

--- hero_game.py
+++ hero_game.py
@@ -8,8 +8,7 @@ valid_actions = {"directions": ["north", "south", "east", "west"],
 # print a list a valid actions before user input. Organized according to
 # possible direction and actions
 for user_action in valid_actions:
-    # if the user chooses the directons option, then it
-    # prints out a list of valid directions
+    # prints out a list of valid directions from the list in valid_actions
     if user_action == "directions":
         print(f"Go in one of the following {user_action}:")
         # define possible_directions as the list that is the value
@@ -19,17 +18,12 @@ for user_action in valid_actions:
         for direction in possible_directions:
             print(f"* {direction}")
     elif user_action == "actions":
-        # if the user chooses the actions option, then it
-        # prints out a list of valid actions
+        # prints out a list of valid actions from the list in valid_actions
         print(f"Complete one of the following {user_action}:")
         possible_actions = valid_actions["actions"]
         # using a loop, print out all the actions the user can use
         for action in possible_actions:
             print(f"* {action}")
-    else:
-        # if the user does not input directions or actions
-        # it prints an "Invalid input" statement
-        print("Invalid input!")
 # after user input, print out the action choosen by the user
 action_input = input("Action: ")
 direction = valid_actions["directions"]
